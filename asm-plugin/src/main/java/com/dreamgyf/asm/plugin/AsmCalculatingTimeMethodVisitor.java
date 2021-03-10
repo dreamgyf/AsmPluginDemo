e18370c3d65962122755fdeb5b2f9b7aa7b5b9de
Gao Yunfeng <g2409197994@gmail.com>
2021-03-10 18:47:27 +0800

    add comment

--- asm-plugin/src/main/java/com/dreamgyf/asm/plugin/AsmCalculatingTimeMethodVisitor.java
+++ asm-plugin/src/main/java/com/dreamgyf/asm/plugin/AsmCalculatingTimeMethodVisitor.java
@@ -43,34 +43,172 @@ class AsmCalculatingTimeMethodVisitor extends MethodVisitor {
 		if (opcode == Opcodes.RETURN) {
 			Label labelEnd = new Label();
 
+			/*
+			   假设此时栈为空
+			 */
+
+			//aload_0: 将this压入栈顶
 			mv.visitVarInsn(Opcodes.ALOAD, 0);
+			//dup: 将栈顶的值复制一份压入栈顶
 			mv.visitInsn(Opcodes.DUP);
+
+			/*
+			   此时栈内容(以左边为栈顶，右边为栈顶):
+			   [this, this]
+			 */
+
+			//以当前栈顶的值为主体，获取当前类的成员变量_$_timeRecorder，类型为long
+			//相当于this._$_timeRecorder
 			mv.visitFieldInsn(Opcodes.GETFIELD, mClassName, TIMER_NAME, "J");
+
+			/*
+			   此时栈内容(以左边为栈顶，右边为栈顶):
+			   [this._$_timeRecorder, this]
+			 */
+
+			//执行System.currentTimeMillis()，并将返回值压入栈顶
 			mv.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/System", "currentTimeMillis", "()J", false);
+
+			/*
+			   此时栈内容(以左边为栈顶，右边为栈顶):
+			   [System.currentTimeMillis()执行后的结果值, this._$_timeRecorder, this]
+			 */
+
+			//将栈顶两long值相加，并将结果压入栈顶
+			//即this._$_timeRecorder + System.currentTimeMillis
 			mv.visitInsn(Opcodes.LADD);
+
+			/*
+			   此时栈内容(以左边为栈顶，右边为栈顶):
+			   [System.currentTimeMillis() + this._$_timeRecorder, this]
+			 */
+
+			//将栈顶的值存入(栈顶 - 1)._$_timeRecorder中
+			//即this._$_timeRecorder = this._$_timeRecorder + System.currentTimeMillis
 			mv.visitFieldInsn(Opcodes.PUTFIELD, mClassName, TIMER_NAME, "J");
+
+			/*
+			   此时栈为空
+			 */
 
 			//L: 对象类型，以分号结尾，如Ljava/lang/Object;
 			mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
 
+			/*
+			   此时栈内容(以左边为栈顶，右边为栈顶):
+			   [System.out]
+			 */
+
 			//构建字符串
+			//创建一个StringBuilder对象，此时还并没有执行构造方法
 			mv.visitTypeInsn(Opcodes.NEW, "java/lang/StringBuilder");
+			//因为执行构造函数会将栈顶的StringBuilder对象弹出，为了后续能继续使用这个对象，所以这里需要先复制一份
 			mv.visitInsn(Opcodes.DUP);
+
+			/*
+			   此时栈内容(以左边为栈顶，右边为栈顶):
+			   [StringBuilder, StringBuilder, System.out]
+			 */
+
+			//以栈顶的StringBuilder调用构造方法
 			mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/StringBuilder", "<init>", "()V", false);
+
+			/*
+			   此时栈内容(以左边为栈顶，右边为栈顶):
+			   [StringBuilder, System.out]
+			 */
+
+			//将常量压入栈顶
 			mv.visitLdcInsn("Time spent: ");
+
+			/*
+			   此时栈内容(以左边为栈顶，右边为栈顶):
+			   ["Time spent: ", StringBuilder, System.out]
+			 */
+
+			//以栈顶的值为参数，(栈顶 - 1)的引用为主体执行StringBuilder.append()方法，将返回值压入栈顶
 			mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(Ljava/lang/String;)Ljava/lang/StringBuilder;", false);
+
+			/*
+			   此时栈内容(以左边为栈顶，右边为栈顶):
+			   [StringBuilder, System.out]
+			 */
+
+			//将this压入栈顶
 			mv.visitVarInsn(Opcodes.ALOAD, 0);
+
+			/*
+			   此时栈内容(以左边为栈顶，右边为栈顶):
+			   [this, StringBuilder, System.out]
+			 */
+
+			//以当前栈顶的值为主体，获取当前类的成员变量_$_timeRecorder，类型为long
+			//相当于this._$_timeRecorder
 			mv.visitFieldInsn(Opcodes.GETFIELD, mClassName, TIMER_NAME, "J");
+
+			/*
+			   此时栈内容(以左边为栈顶，右边为栈顶):
+			   [this._$_timeRecorder, StringBuilder, System.out]
+			 */
+
+			//以栈顶的值为参数，(栈顶 - 1)的引用为主体执行StringBuilder.append()方法，将返回值压入栈顶
 			mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(J)Ljava/lang/StringBuilder;", false);
+
+			/*
+			   此时栈内容(以左边为栈顶，右边为栈顶):
+			   [StringBuilder, System.out]
+			 */
+
+			//将常量压入栈顶
 			mv.visitLdcInsn("ms, when " + mFormatClassName + "." + mMethodName + ":" + mMethodDescriptor);
+
+			/*
+			   此时栈内容(以左边为栈顶，右边为栈顶):
+			   [字符串常量, StringBuilder, System.out]
+			 */
+
+			//以栈顶的值为参数，(栈顶 - 1)的引用为主体执行StringBuilder.append()方法，将返回值压入栈顶
 			mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(Ljava/lang/String;)Ljava/lang/StringBuilder;", false);
+
+			/*
+			   此时栈内容(以左边为栈顶，右边为栈顶):
+			   [StringBuilder, System.out]
+			 */
+
+			//以栈顶的值为主体，执行StringBuilder.toString()方法，将返回值压入栈顶
 			mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/StringBuilder", "toString", "()Ljava/lang/String;", false);
+
+			/*
+			   此时栈内容(以左边为栈顶，右边为栈顶):
+			   [String, System.out]
+			 */
+
+			//以栈顶的值为参数，(栈顶 - 1)的引用为主体执行PrintStream.println()方法
+			//相当于System.out.println(String)
 			mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
 
+			/*
+			   此时栈为空
+			 */
+
+			//执行Thread.currentThread()，并将返回值压入栈顶
 			mv.visitMethodInsn(Opcodes.INVOKESTATIC, "java/lang/Thread", "currentThread", "()Ljava/lang/Thread;", false);
+
+			/*
+			   此时栈内容(以左边为栈顶，右边为栈顶):
+			   [Thread.currentThread()执行的结果]
+			 */
+
+			//以栈顶的值为主体，执行getStackTrace()方法，将返回值压入栈顶
+			//相当于Thread.currentThread().getStackTrace()
 			mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/Thread", "getStackTrace", "()[Ljava/lang/StackTraceElement;", false);
 
-			//StackTraceElement数组
+			/*
+			   此时栈内容(以左边为栈顶，右边为栈顶):
+			   [StackTraceElement数组]
+			 */
+
+			//使用一个临时变量保存StackTraceElement数组
 			mv.visitVarInsn(Opcodes.ASTORE, 2);
 			mv.visitVarInsn(Opcodes.ALOAD, 2);
 			//StackTraceElement数组备份
